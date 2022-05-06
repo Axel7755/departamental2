8530e5ae67425ae54f813c3f44eb43c06e865db5
Axel77755 <baxeltomas@gmail.com>
2022-05-06 16:15:09 -0500

    nuevo

--- AdminLTE-3.2.0/AdminLTE-3.2.0/jspropios/calculadora_Avansada.js
+++ AdminLTE-3.2.0/AdminLTE-3.2.0/jspropios/calculadora_Avansada.js
@@ -1,31 +1,39 @@
-function Suma(){
-    
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
-    let suma = Number(num1) + Number(num2);
-    alert("La suma es: "+suma);
-}
-function Resta(){
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
-    let suma = num1 - num2;
-    alert("La suma es: "+suma);
-}
-function Multiplicacion(){
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
-    let suma = num1 * num2;
-    alert("La suma es: "+suma);
-}
-function Division(){
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
+aux=0;
+let display = document.getElementById("display");
+
+function agregarDijito(dijito){
+    if(aux==0){
+        display.value = dijito;
+        aux=1;
+    }else{
+        display.value = display.value+dijito;
+    }
+    /*let num2 = document.getElementById("num2").value;
     let suma = num1 / num2;
-    alert("La suma es: "+suma);
+    alert("La suma es: "+suma);*/
 }
-function OPeraciones(){
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
-    let suma = num1 / num2;
-    alert("La suma es: "+suma);
+function Clear(){
+    display.value = 0;
+    aux=0;
+}
+function CalcularResultado(){
+    display.value = eval(display.value);
+    aux=0;
+}
+function Potencia(){
+    let exponente = document.getElementById("elevar_potencia").value;
+    display.value = Math.pow(display.value,exponente);
+}
+function Raiz(){
+    display.value = Math.sqrt(display.value);
+}
+function Factorial(){
+    var total = 1; 
+	for (i=1; i<=display.value; i++) {
+		total = total * i; 
+	}
+    display.value = total; 
+}
+function LogN(){
+    display.value = Math.log(display.value);
 }
